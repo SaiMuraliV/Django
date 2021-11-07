77247a9b92303f5c2f3dd2fc97005c9103fc2de6
Murali <sai.murali16@gmail.com>
2021-11-07 17:32:46 +0530

    Registration

---
+++ Django_level_five/learning_users/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from basic_app import views
+
+# app name for
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+]
+
+
